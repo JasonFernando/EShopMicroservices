9ce27f441ee9579be63a758b56b9fae422530bf2
Jason Fernando Aponte Tapahuasco <jason.aponte@globant.com>
2026-01-07 16:39:49 -0500

    Implementation CQRS, Carter library, mapster library

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public class CreateProductHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // Business logic to create a product
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageUrl = command.ImageUrl,
+                Price = command.Price
+            };
+
+            //TO DO
+            //save database
+
+            //return result
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageUrl, decimal Price)
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+}
