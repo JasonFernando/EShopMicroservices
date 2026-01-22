ea6bf64f9ab73b61e884d9054ec1fa1f816bb4ca
Jason Aponte <jason_at@hotmail.es>
2026-01-22 06:51:10 -0500

    Update docker-compose , implement GetProductsQueryHandler

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+    public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IqueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductHanlder called with {@Query}", query);
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResult(products);
+        }
+    }
+}
